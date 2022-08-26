872934706f3353c8264e0904142fc715e157956a
AgustinGallotti <AgustinGallotti>
2022-08-26 02:25:45 +0000

    check

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ import datetime
 def do_pack():
     """generate a tgz archives from contents of the web_static"""
     try:
-        time= datetime.datetime.now()
+        time = datetime.datetime.now()
         date = time.strftime("%Y%m%d%H%M%S")
         local("mkdir -p versions")
         local("tar -cfvz versions/web_static_{}.tgz web_static/".format(date))
