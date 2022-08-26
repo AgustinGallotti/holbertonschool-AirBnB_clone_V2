a7b826bcfde9a11b9c836899b99655ec447b9fd2
AgustinGallotti <AgustinGallotti>
2022-08-26 01:47:38 +0000

    check

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployement of web_static
-server {
-  root /data;
-
-  location /web_static/releases {
-    try_files $uri /web_static/releases;
-  }
-
-  location /web_static/shared {
-    try_files $uri /web_static/shared;
-  }
-
-  location /web_static/releases/test {
-    try_files $uri /web_static/releases/test;
-  }
-
-  location /web_static/releases/test/index.html {
-    alias /data/web_static/releses/test/index.html;
-    try_files $uri /web_static/releases/test/index.html;
-  }
-}
-LN -s /data/web_static/current/ /data/web_static/releses/test/
-#GRANTS owner /data/ USER ubuntu
-sudo chown -R www-data:www-data ubuntu /data/web_static_releases/test/index.html
-location /index.html/ { alias /data/web_static/releases/test/index.html; }
+apt-get update
+apt-get install nginx -y
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/current/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
+sed -i "/listen 80 default_server;/a location /hbnb_static/ {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 service nginx restart
+exit 0
