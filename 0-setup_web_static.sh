f5b1d2093de36cc28da7462484cd4acc53bbff29
AgustinGallotti <AgustinGallotti>
2022-08-26 01:53:48 +0000

    check

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,6 @@ mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/current/
 echo "<html>
   <head>
   </head>
@@ -19,4 +18,3 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i "/listen 80 default_server;/a location /hbnb_static/ {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 service nginx restart
-exit 0
