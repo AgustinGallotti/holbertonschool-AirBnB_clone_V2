3537b2a25c5c5546df9a3b2165f57fdf1ee937df
AgustinGallotti <AgustinGallotti>
2022-08-26 02:28:56 +0000

    check

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,17 +3,18 @@
     module
 """
 
+
 from fabric.api import local
 import datetime
 
 
 def do_pack():
-    """generate a tgz archives from contents of the web_static"""
+    """generates a .tgz archive from the contents of the web_static folder"""
     try:
         time = datetime.datetime.now()
         date = time.strftime("%Y%m%d%H%M%S")
         local("mkdir -p versions")
-        local("tar -cfvz versions/web_static_{}.tgz web_static/".format(date))
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(date))
         return("versions/web_static_{}.tgz".format(date))
-    except Exception:
+    except exception:
         return None
