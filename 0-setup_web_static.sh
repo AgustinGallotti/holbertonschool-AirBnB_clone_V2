f689e702a81537676f088ae26475d437de34d805
AgustinGallotti <AgustinGallotti>
2022-08-26 00:53:42 +0000

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployement of web_static
-CREATE /data/ IF NOT EXISTS
-CREATE /data/web_static/ IF NOT EXISTS
-CREATE /data/web_static/releases/ IF NOT EXISTS
-CREATE /data/web_static/releases/test/ IF NOT EXISTS
-CREATE /data/web_static/releases/test/index.html IF NOT EXISTS
-LN -s /data/web_static/releases/test/ /test/
+server {
+  root /data;
+
+  location /web_static/releases {
+    try_files $uri /web_static/releases;
+  }
+
+  location /web_static/shared {
+    try_files $uri /web_static/shared;
+  }
+
+  location /web_static/releases/test {
+    try_files $uri /web_static/releases/test;
+  }
+
+  location /web_static/releases/test/index.html {
+    alias /data/web_static/releses/test/index.html;
+    try_files $uri /web_static/releases/test/index.html;
+  }
+}
+LN -s /data/web_static/current/ /data/web_static/releses/test/
 #GRANTS owner /data/ USER ubuntu
-sudo chown 755 ubuntu /data/web_static_releases/test/
-UPDATE nginx 
+sudo chown -R www-data:www-data ubuntu /data/web_static_releases/test/index.html
+location /index.html/ { alias /data/web_static/releases/test/index.html; }
+service nginx restart
