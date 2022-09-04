d1e09f497b5531484252d56f6148aad1af8c8509
AgustinGallotti <AgustinGallotti>
2022-09-04 01:05:32 +0000

    check

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" route """
+
+
+if __name__ == "__main__":
+	from flask import Flask
+
+	app = Flask(__name__)
+
+	@app.route('/', strict_slashes=False)
+	def hello_hbnb():
+		"""root folder route"""
+	return "Hello HBNB!"
+
+	@app.route('/hbnb', strict_slashes=False)
+	def hbnb_route():
+		"""/hbnb/ folder route"""
+	return "HBNB"
+
+	@ap.route('/c/<text>', strict_slashes=False)
+		"""some text in uotput depending on url after /c/"""
+		return "C " + text.replace('_', ' ')
+
+	app.run(host='0.0.0.0')
