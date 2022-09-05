7c4ea1c364216240e3cee6bbeaebc015606ebfe5
AgustinGallotti <AgustinGallotti>
2022-09-05 22:56:00 +0000

    helo hbnb

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,13 @@
 
 
 if __name__ == "__main__":
-	from flask import Flask
+    from flask import Flask
 
-	app = Flask(__name__)
+    app = Flask(__name__)
 
-	@app.route('/', strict_slashes=False)
-	def routte():
-		"""root folder route"""
-		return "Hello HBNB!"
+    @app.route('/', strict_slashes=False)
+    def routte():
+        """root folder route"""
+        return "Hello HBNB!"
 
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
