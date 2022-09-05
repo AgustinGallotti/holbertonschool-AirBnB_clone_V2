7d29382308e5a0e2fb7723948a9758fad01346e0
AgustinGallotti <AgustinGallotti>
2022-09-05 22:54:32 +0000

    check

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,18 +3,18 @@
 
 
 if __name__ == '__main__':
-	from flask import Flask
+    from flask import Flask
 
-	app = Flask(__name__)
+    app = Flask(__name__)
 
-	@app.route('/', strict_slashes=False)
-	def routte():
-		"""root folder route"""
-		return "Hello HBNB!"
+    @app.route('/', strict_slashes=False)
+    def routte():
+        """root folder route"""
+        return "Hello HBNB!"
 
-	@app.route('/hbnb', strict_slashes=False)
-	def hbnb():
-		"""hbnbb"""
-		return "HBNB"
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """hbnbb"""
+        return "HBNB"
 
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
